3cdc9372082f3865e6822128d72c818953c1e7ac
bruunooAlmeida <almeidabrunoti@gmail.com>
2022-09-21 12:59:16 -0300

    Conclusão do Desafiop

--- src/global.js
+++ src/global.js
@@ -1,9 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-    *,body{
-        margin:0;
-        padding:0;        
-    }
+  *, body {
+    margin: 0;
+    padding: 0;
+  }
 `
-    
